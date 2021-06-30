73c6bb3e5f912fe0526dc243500c34013f5b5c16
Ahamed Fahmi <76228369+ahamedfahmi@users.noreply.github.com>
2021-06-30 12:08:25 +0530

    Home Work #2
    Description About a song

---
+++ Home_Work/2/song.kt
@@ -0,0 +1,36 @@
+fun main() 
+{
+/* 
+     In this code I'm going describe a song
+     Which is "Shape Of you"
+*/
+
+println("----------- This is a description of 'Shape Of You' Song -----------\n\n")
+
+
+//The Artist Name
+val Artist: String = "Ed Sheeran"
+println("Artist name is $Artist")
+
+//The Song Name
+val Name: String = "Shape Of You"
+println("Name of the song is $Name")
+
+//The released year
+val Year: Int = 2017
+println("Released year is $Year")
+
+// The Genre 
+val Genre: String = "Pop"
+println("Genre of the song is $Genre")
+
+// Song Duration
+val Duration: Double = 4.23
+println("Duration of the song is $Duration")
+
+//The lyricist of the song
+val Lyricist: String = "Ed Sheeran"
+println("The lyricist of this song is $Lyricist")
+
+
+}
